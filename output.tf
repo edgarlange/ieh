c2562af84faaf5ae47490330912d2f736eecc4d5
Edgar Lange <edgar_lange@outlook.com>
2023-08-16 13:24:49 -0500

    Ajuste output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,18 @@ output "database_id" {
   value = azurerm_mssql_database.ieh_db.id
 }
 
+output "database_name" {
+  value = azurerm_mssql_database.ieh_db.name
+}
+
+output "storage_name" {
+  value = azurerm_storage_account.ieh_00.name
+}
+
+output "webapp_name" {
+  value = azurerm_windows_web_app.ieh_webapp.name
+}
+
 # output "tls_private_key" {
 #   value     = tls_private_key.example_ssh.private_key_pem
 #   sensitive = true
