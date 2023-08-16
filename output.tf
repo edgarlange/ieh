483de4579829d9f41a550c9d1973565d492d3221
Edgar Lange <edgar_lange@outlook.com>
2023-08-16 14:32:29 -0500

    Ajuste output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "database_name" {
-  value = azurerm_mssql_server.ieh_db.fully_qualified_domain_name
+  value = azurerm_mssql_server.ieh_000.fully_qualified_domain_name
 }
 
 output "storage_name" {
